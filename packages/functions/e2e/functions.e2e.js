d2494abf4838ce1f0e349badacd21882d4359f80
Pavan Welihinda <pavan@babyjourney.se>
2022-09-06 08:13:26 +0530

    feat: message deliveries to bigquery support for android

--- packages/functions/e2e/functions.e2e.js
+++ packages/functions/e2e/functions.e2e.js
@@ -15,7 +15,77 @@
  *
  */
 
-const SAMPLE_DATA = require('../../../.github/workflows/scripts/functions/lib/sample-data').default;
+// const SAMPLE_DATA = require('../../../.github/workflows/scripts/functions/src/sample-data').default;
+const SAMPLE_DATA = {
+  number: 1234,
+  string: 'acde',
+  boolean: true,
+  null: null,
+  object: {
+    number: 1234,
+    string: 'acde',
+    boolean: true,
+    null: null,
+  },
+  array: [1234, 'acde', true, null],
+  deepObject: {
+    array: [1234, 'acde', false, null],
+    object: {
+      number: 1234,
+      string: 'acde',
+      boolean: true,
+      null: null,
+      array: [1234, 'acde', true, null],
+    },
+    number: 1234,
+    string: 'acde',
+    boolean: true,
+    null: null,
+  },
+  deepArray: [
+    1234,
+    'acde',
+    true,
+    null,
+    [1234, 'acde', true, null],
+    {
+      number: 1234,
+      string: 'acde',
+      boolean: true,
+      null: null,
+      array: [1234, 'acde', true, null],
+    },
+  ],
+  deepMap: {
+    number: 123,
+    string: 'foo',
+    booleanTrue: true,
+    booleanFalse: false,
+    null: null,
+    list: ['1', 2, true, false],
+    map: {
+      number: 123,
+      string: 'foo',
+      booleanTrue: true,
+      booleanFalse: false,
+      null: null,
+    },
+  },
+  deepList: [
+    '1',
+    2,
+    true,
+    false,
+    ['1', 2, true, false],
+    {
+      number: 123,
+      string: 'foo',
+      booleanTrue: true,
+      booleanFalse: false,
+      null: null,
+    },
+  ],
+};
 
 describe('functions()', function () {
   describe('namespace', function () {
